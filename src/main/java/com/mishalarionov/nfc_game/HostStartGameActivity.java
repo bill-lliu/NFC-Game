9a82d64b8927189e994c033197999f15bf44cf51
alstonlo <40709307+alstonlo@users.noreply.github.com>
2018-10-14 09:14:41 -0400

    added various activities

--- src/main/java/com/mishalarionov/nfc_game/HostStartGameActivity.java
+++ src/main/java/com/mishalarionov/nfc_game/HostStartGameActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class HostStartGameActivity extends AppCompatActivity implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,12 +15,20 @@ public class HostStartGameActivity extends AppCompatActivity implements View.OnC
         setContentView(R.layout.activity_host_game);
 
         //buttons
-        findViewById(R.id.joinButton).setOnClickListener(this);
-        findViewById(R.id.hostButton).setOnClickListener(this);
+        findViewById(R.id.hostStartButton).setOnClickListener(this);
         //updateUI(currentUser);
     }
 
-    protected void onClick(View v){
-
+    public void onClick(View v){
+        int i = v.getId();
+        if (i == R.id.hostStartButton){
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference("https://nfc-game-933e3.firebaseio.com/");
+            myRef.setValue(findViewById(R.id.question1Field).qetText());
+            myRef = database.getReference("Question 2");
+            myRef.setValue(findViewById(R.id.question2Field).getText());
+            myRef = database.getReference("Question 3");
+            myRef.setValue(findViewById(R.id.question3Field).getText());
+        }
     }
 }
---
+++ src/main/java/com/mishalarionov/nfc_game/InitializeActivity.java
@@ -0,0 +1,53 @@
+package com.mishalarionov.nfc_game;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class InitializeActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intialize_game);
+        final String question1;
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("server/"); //WHAT IS OUR SERVER?
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                question1
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+        TextView question1 = findViewById(R.id.question1);
+        question1.setText();
+        //buttons
+        findViewById(R.id.confirmButton).setOnClickListener(this);
+        //updateUI(currentUser);
+    }
+
+    public void onClick(View v) {
+        int i = v.getId();
+    }
+
+
+    ;
+      mPostReference.addValueEventListener(postListener);
+
+}
